8522ebf64198267e4c41c545263066525543a7a9
Paul Hunt <woodburysoft@gmail.com>
2017-04-21 11:29:03 +0100

    Sdelete commented out for testing purposes

--- scripts/compact.bat
+++ scripts/compact.bat
@@ -28,5 +28,5 @@ net start wuauserv
 
 cmd /c C:\Windows\Temp\ultradefrag-portable-6.1.0.amd64\udefrag.exe --optimize --repeat C:
 
-cmd /c %SystemRoot%\System32\reg.exe ADD HKCU\Software\Sysinternals\SDelete /v EulaAccepted /t REG_DWORD /d 1 /f
-cmd /c C:\Windows\Temp\sdelete.exe -q -z C:
+REM cmd /c %SystemRoot%\System32\reg.exe ADD HKCU\Software\Sysinternals\SDelete /v EulaAccepted /t REG_DWORD /d 1 /f
+REM cmd /c C:\Windows\Temp\sdelete.exe -q -z C:
